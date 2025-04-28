a8db9bfd51e9cb1316eeefa40d928df0332ad8cc
aniket <aniket@gamil.com>
2025-04-28 16:18:25 +0530

    updating index.js

--- api/index.js
+++ api/index.js
@@ -183,6 +183,6 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*frontend', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
